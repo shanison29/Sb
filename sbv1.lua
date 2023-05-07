2db066777fcca41825540d8179163deead35fcc4
shanison29 <shan29ison@gmail.com>
2023-05-07 19:36:03 +0800

    Update sbv1.lua
    queueteleport([[
    loadstring(game:HttpGet('https://raw.githubusercontent.com/shanison29/Sb/main/sbv1.lua'))()
    ]])

--- sbv1.lua
+++ sbv1.lua
@@ -337,7 +337,7 @@ end)
 queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
 
 queueteleport([[
-    print("Hello world!")
+loadstring(game:HttpGet('https://raw.githubusercontent.com/shanison29/Sb/main/sbv1.lua'))()
 ]])
 
 ---------------Player Count on GUI-------------
